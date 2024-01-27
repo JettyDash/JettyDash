5aece00eaa4ed9d78d5b908f2e928d0c21ae7408
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-27 20:39:31 +0300

    Connection Entities:BackendApplication/Api/Startup.cs

--- BackendApplication/Api/Startup.cs
+++ BackendApplication/Api/Startup.cs
@@ -1,6 +1,76 @@
+using Infrastructure.DbContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 namespace Api;
 
 public class Startup
 {
-    
+    public IConfiguration Configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        
+        
+        services.AddDbContext<BackendDbContext>(options =>
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        });
+        
+        // // MediatR
+        // foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+        // {
+        //     services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assembly));
+        // }
+        //
+        // // AutoMapper
+        // var mapperConfig = new MapperConfiguration(cfg => cfg.AddProfile(new MapperConfig()));
+        // services.AddSingleton(mapperConfig.CreateMapper());
+        //
+
+        
+        services.AddEndpointsApiExplorer();
+        // services.AddHttpContextAccessor();
+        
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "JettyDash Api", Version = "v1.0" });
+
+        });
+
+        services.AddHealthChecks();
+        services.AddControllers();
+        
+
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHealthChecks("/health");
+        // app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
+
+
+        app.UseAuthentication();
+        app.UseRouting();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
 }
