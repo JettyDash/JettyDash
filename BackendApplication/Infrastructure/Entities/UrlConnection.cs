c530e1590970dfe83924a3425ad358f4e7238ba8
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-27 20:39:31 +0300

    Connection Entities:BackendApplication/Infrastructure/Entities/UrlConnection.cs

--- BackendApplication/Infrastructure/Entities/UrlConnection.cs
+++ BackendApplication/Infrastructure/Entities/UrlConnection.cs
@@ -1,67 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace Infrastructure.Entities;
 
 public class UrlConnection : BaseConnection
 {
-    public int ExpenseRequestId { get; set; } // (Primary Key)
-    public int UserId { get; set; } // (Foreign Key)
-    public int CreatedBy { get; set; }
-    public double Amount { get; set; }
-    public int CategoryId { get; set; } // (Foreign Key to ExpenseCategory)
-    public string PaymentMethod { get; set; }
-    public string PaymentLocation { get; set; }
-    public string Documents { get; set; } //  (File upload or references)
-    public ExpenseRequestStatus Status { get; set; } = ExpenseRequestStatus.Pending; // (Pending, Approved, Rejected)
-    public string Description { get; set; } //  (from User)
-    public DateTime CreationDate { get; set; }
-    public DateTime LastUpdateTime { get; set; }
-    public PaymentRequestStatus PaymentStatus { get; set; } = PaymentRequestStatus.Pending;// (Pending, Declined, Completed, Failed)
-    
-    public DateTime? PaymentDate { get; set; } = null; // (if Status is Completed)
-    public string PaymentDescription { get; set; } = Constants.DefaultValues.PaymentDescription;//  (if Status is Rejected)
-    public virtual User User { get; set; }
-    public virtual ExpenseCategory ExpenseCategory { get; set; }
+    public string Url { get; set; }
+
 }
 
-public class ExpenseRequestConfiguration : IEntityTypeConfiguration<Expense>
+public class UrlConnectionConfiguration : BaseConnectionConfiguration<UrlConnection>, IEntityTypeConfiguration<UrlConnection>
 {
-    public void Configure(EntityTypeBuilder<Expense> builder)
+    public void Configure(EntityTypeBuilder<UrlConnection> builder)
     {
-        builder.HasKey(e => e.ExpenseRequestId);
-        builder.HasIndex(e => e.ExpenseRequestId).IsUnique();
-        
-        builder.Property(e => e.UserId).IsRequired();
-        
-        builder.Property(e => e.CreatedBy).IsRequired();
-
-        builder.Property(e => e.Amount).IsRequired();
-        
-        builder.Property(e => e.CategoryId).IsRequired();
-
-        builder.Property(e => e.PaymentMethod).IsRequired().HasMaxLength(50);
-
-        builder.Property(e => e.PaymentLocation).IsRequired().HasMaxLength(255);
-
-        builder.Property(e => e.Documents).HasMaxLength(255); // Optional
-
-        builder.Property(e => e.Status).IsRequired().HasConversion<string>();
-
-        builder.Property(e => e.Description).IsRequired().HasMaxLength(255);
-
-        builder.Property(e => e.CreationDate).IsRequired();
-        
-        builder.Property(e => e.LastUpdateTime).IsRequired();
-        
-        builder.Property(e => e.PaymentStatus).IsRequired().HasConversion<string>();
+        base.Configure(builder);
+        builder.Property(e => e.Url).IsRequired();
 
         builder.HasOne(e => e.User)
-            .WithMany(u => u.ExpenseRequests)
+            .WithMany(u => u.UrlConnections)
             .HasForeignKey(e => e.UserId);
-
-        builder.HasOne(e => e.ExpenseCategory)
-            .WithMany()
-            .HasForeignKey(e => e.CategoryId);
-            
-
-        
     }
 }
