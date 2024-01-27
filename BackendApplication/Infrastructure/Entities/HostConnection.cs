0b6990fa28030ab8a797e161370c1106bbfd5c72
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-27 20:39:31 +0300

    Connection Entities:BackendApplication/Infrastructure/Entities/HostConnection.cs

--- BackendApplication/Infrastructure/Entities/HostConnection.cs
+++ BackendApplication/Infrastructure/Entities/HostConnection.cs
@@ -1,6 +1,29 @@
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace Infrastructure.Entities;
 
-public class HostConnection
+public class HostConnection : BaseConnection
 {
-    
+    public int Port { get; set; }
+    public string Host { get; set; }
+    public string DatabaseOrSchema { get; set; }
+}
+
+public class HostConnectionConfiguration : BaseConnectionConfiguration<HostConnection>, IEntityTypeConfiguration<HostConnection>
+{
+    public void Configure(EntityTypeBuilder<HostConnection> builder)
+    {
+        base.Configure(builder);
+        builder.Property(e => e.Port).IsRequired();
+        
+        builder.Property(e => e.Host).IsRequired().HasMaxLength(255);
+        
+        builder.Property(e => e.DatabaseOrSchema).IsRequired().HasMaxLength(255);
+
+        builder.HasOne(e => e.User)
+            .WithMany(u => u.HostConnections)
+            .HasForeignKey(e => e.UserId);
+    }
 }
