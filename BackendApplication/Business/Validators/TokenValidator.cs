0bf0266ca0d36063e0848d4f25853730813148ca
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-29 00:59:13 +0300

    Jwt Token and RBAC:BackendApplication/Business/Validators/TokenValidator.cs

--- BackendApplication/Business/Validators/TokenValidator.cs
+++ BackendApplication/Business/Validators/TokenValidator.cs
@@ -7,7 +7,7 @@ public class CreateTokenValidator : AbstractValidator<TokenRequest>
 {
     public CreateTokenValidator()
     {
-        RuleFor(x => x.UserName).NotEmpty().MaximumLength(50);
+        RuleFor(x => x.Username).NotEmpty().MaximumLength(50);
         RuleFor(x => x.Password).NotEmpty().MaximumLength(255);
     }
 }
