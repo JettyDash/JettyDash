1b1b20640cd1b644049791abb4248caffb5c4de8
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-28 13:23:43 +0300

    Vault InÄitialize:BackendApplication/Business/Cqrs/ConnectionCqrs.cs

--- BackendApplication/Business/Cqrs/ConnectionCqrs.cs
+++ BackendApplication/Business/Cqrs/ConnectionCqrs.cs
@@ -1,12 +1,17 @@
 using MediatR;
+using Schemes.Dtos;
 
 namespace Business.Cqrs;
 
 
+// Connection Respose base responsetan kalıtım alacak apiresponse parametrelerini eklersin
+public record CreateUrlConnectionCommand(CreateUrlConnectionRequest Model) : IRequest<ConnectionResponse>;
+public record CreateHostConnectionCommand(CreateHostConnectionRequest Model) : IRequest<ConnectionResponse>;
 
-public record DeleteAllConnectionCommand(int ExpenseRequestId) : IRequest<ExpenseResponse>;
+public record UpdateUrlConnectionCommand(int ConnectionId, UpdateUrlConnectionRequest Model) : IRequest<ConnectionResponse>;
+public record UpdateHostConnectionCommand(int ConnectionId, UpdateHostConnectionRequest Model) : IRequest<ConnectionResponse>;
 
+public record DeleteConnectionCommand(int ConnectionId) : IRequest<ConnectionResponse>;
 
-public record GetAllConnectionQuery() : IRequest<List<ExpenseResponse>>;
+public record GetAllConnectionQuery() : IRequest<List<ConnectionResponse>>;
 
-// public record GetAllConnectionByParameterQuery(GetExpenseByParameterRequest Model) : IRequest<List<ExpenseResponse>>;
