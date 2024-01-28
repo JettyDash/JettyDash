97979982f36f55e8a24bd884165ddb8e88f869b5
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-28 13:23:43 +0300

    Vault InÄitialize:BackendApplication/Schemes/Dtos/ConnectionDtos.cs

--- BackendApplication/Schemes/Dtos/ConnectionDtos.cs
+++ BackendApplication/Schemes/Dtos/ConnectionDtos.cs
@@ -4,23 +4,23 @@ namespace Schemes.Dtos;
 
 public class ConnectionRequestBase
 {
-    public string ConnectionType { get; set; }
-    public string DatabaseName { get; set; }
-    public string Type { get; set; }
-    public string Username { get; set; }
-    public string Password { get; set; }
+    public string? ConnectionType { get; set; }
+    public string? DatabaseName { get; set; }
+    public string? DatabaseType { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
 }
 
 public abstract class CreateUrlConnectionRequest : ConnectionRequestBase
 {
-    public string Url { get; set; }
+    public string? Url { get; set; }
 }
 
 public abstract class CreateHostConnectionRequest : ConnectionRequestBase
 {
-    public string Host { get; set; }
-    public string Port { get; set; }
-    public string DatabaseOrSchema { get; set; }
+    public string? Host { get; set; }
+    public int? Port { get; set; }
+    public string? DatabaseOrSchema { get; set; }
 }
 
 public abstract class UpdateUrlConnectionRequest : CreateUrlConnectionRequest
@@ -36,7 +36,7 @@ public class ConnectionResponseBase
     public string ConnectionType { get; set; }
     public int UserId { get; set; }
     public string DatabaseName { get; set; }
-    public string Type { get; set; }
+    public string DatabaseType { get; set; }
     
     public string Status { get; set; }
     public DateTime CreationDate { get; set; }
