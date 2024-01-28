3a1375f70c3867bf7299022b8140e7aa6f8b61f4
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-28 13:23:43 +0300

    Vault InÄitialize:BackendApplication/Schemes/Vault/VaultConfig.cs

--- BackendApplication/Schemes/Vault/VaultConfig.cs
+++ BackendApplication/Schemes/Vault/VaultConfig.cs
@@ -2,5 +2,8 @@ namespace Schemes.Vault;
 
 public class VaultConfig
 {
+    public string Address { get; set; }
+    public string Token { get; set; }
+    public int EngineVersion { get; set; }
     
 }
