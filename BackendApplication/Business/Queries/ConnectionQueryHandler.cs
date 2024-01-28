ee9749f02847e0c9a3c118967e9a85d3f9c81225
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-29 00:59:13 +0300

    Jwt Token and RBAC:BackendApplication/Business/Queries/ConnectionQueryHandler.cs

--- BackendApplication/Business/Queries/ConnectionQueryHandler.cs
+++ BackendApplication/Business/Queries/ConnectionQueryHandler.cs
@@ -1,6 +1,41 @@
+using Business.Cqrs;
+using Business.Validators;
+using AutoMapper;
+using Business.Services;
+using Infrastructure.DbContext;
+using Infrastructure.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Schemes.Dtos;
+
 namespace Business.Queries;
 
-public class ConnectionQueryHandler
+public class ConnectionQueryHandler :
+    IRequestHandler<GetAllConnectionQuery, ApiResponse<List<ConnectionResponse>>>
+
 {
+    private readonly BackendDbContext dbContext;
+    private readonly IMapper mapper;
+    private readonly IHandlerValidator validate;
+    private readonly IUserService userService;
     
+    public ConnectionQueryHandler(BackendDbContext dbContext, IMapper mapper, IHandlerValidator validate, IUserService userService)
+    {
+        this.dbContext = dbContext;
+        this.mapper = mapper;
+        this.validate = validate;
+        this.userService = userService;
+    }
+
+    public async Task<ApiResponse<List<ConnectionResponse>>> Handle(GetAllConnectionQuery request, CancellationToken cancellationToken)
+    {
+        var list = await dbContext.Set<Connection>()
+        .Where(conn => conn.UserId == userService.GetUserId())
+        .ToListAsync(cancellationToken);
+        
+        var mappedList = mapper.Map<List<Connection>, List<ConnectionResponse>>(list);
+        
+        return new ApiResponse<List<ConnectionResponse>>(mappedList);
+    }
 }
