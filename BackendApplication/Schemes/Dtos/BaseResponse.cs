7631dff608e4aad51408b3272364a0a57d710ac7
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-28 13:23:43 +0300

    Vault InÄitialize:BackendApplication/Schemes/Dtos/BaseResponse.cs

--- BackendApplication/Schemes/Dtos/BaseResponse.cs
+++ BackendApplication/Schemes/Dtos/BaseResponse.cs
@@ -1,6 +1,14 @@
+using System.Text.Json;
+
 namespace Schemes.Dtos;
 
-public class BaseResponse
+public class BaseResponse<T>(T content)
 {
-    
+    public DateTime ServerDate { get; set; } = DateTime.UtcNow;
+    public Guid ReferenceNo { get; set; } = Guid.NewGuid();
+    public bool Success { get; set; } = true;
+    public string Message { get; set; } = "Success";
+    public T Content { get; set; } = content;
+
+    public override string ToString() => JsonSerializer.Serialize(this);
 }
