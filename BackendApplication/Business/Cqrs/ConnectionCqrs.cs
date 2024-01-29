acc4b77dcce4a779507363674d0e166086b99d1d
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-29 23:29:45 +0300

    dapper:BackendApplication/Business/Cqrs/ConnectionCqrs.cs

--- BackendApplication/Business/Cqrs/ConnectionCqrs.cs
+++ BackendApplication/Business/Cqrs/ConnectionCqrs.cs
@@ -10,10 +10,12 @@ public record TestConnectionCommand(int ConnectionId) : IRequest<ApiResponse<Con
 public record CreateUrlConnectionCommand(CreateUrlConnectionRequest Model) : IRequest<ApiResponse<ConnectionResponse>>;
 public record CreateHostConnectionCommand(CreateHostConnectionRequest Model) : IRequest<ApiResponse<ConnectionResponse>>;
 
+
+// TODO: Check nullable working properly
 public record UpdateUrlConnectionCommand(int ConnectionId, UpdateUrlConnectionRequest Model) : IRequest<ApiResponse<ConnectionResponse>>;
 public record UpdateHostConnectionCommand(int ConnectionId, UpdateHostConnectionRequest Model) : IRequest<ApiResponse<ConnectionResponse>>;
-
 public record DeleteConnectionCommand(int ConnectionId) : IRequest<ApiResponse<ConnectionResponse>>;
+
 
 public record GetAllConnectionQuery() : IRequest<ApiResponse<List<ConnectionResponse>>>;
 
