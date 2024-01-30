72b598951515d8f071e2d3287bce311f246139c0
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-30 21:54:06 +0300

    refactor:BackendApplication/Business/Commands/ConnectionCommandHandler.cs

--- BackendApplication/Business/Commands/ConnectionCommandHandler.cs
+++ BackendApplication/Business/Commands/ConnectionCommandHandler.cs
@@ -7,6 +7,7 @@ using Infrastructure.Entities;
 using MediatR;
 using Schemes.Dtos;
 using Schemes.Enums;
+using Schemes.Exceptions;
 
 namespace Business.Commands;
 
@@ -16,94 +17,119 @@ public class ConnectionCommandHandler(
     IHandlerValidator validate,
     IVaultService vaultService,
     IUserService userService,
-    IDapperService dapperService)
-    :
-        IRequestHandler<TestConnectionCommand, ApiResponse<ConnectionResponse>>,
-        IRequestHandler<CreateUrlConnectionCommand, ApiResponse<ConnectionResponse>>,
-        IRequestHandler<CreateHostConnectionCommand, ApiResponse<ConnectionResponse>>,
-        IRequestHandler<UpdateUrlConnectionCommand, ApiResponse<ConnectionResponse>>,
-        IRequestHandler<UpdateHostConnectionCommand, ApiResponse<ConnectionResponse>>,
-        IRequestHandler<DeleteConnectionCommand, ApiResponse<ConnectionResponse>>
+    IMediator mediator,
+    IDapperService dapperService) :
+    IRequestHandler<TestConnectionCommand, ApiResponse<ConnectionResponse>>,
+    IRequestHandler<CreateUrlConnectionCommand, ApiResponse<ConnectionResponse>>,
+    IRequestHandler<CreateHostConnectionCommand, ApiResponse<ConnectionResponse>>,
+    IRequestHandler<UpdateUrlConnectionCommand, ApiResponse<ConnectionResponse>>,
+    IRequestHandler<UpdateHostConnectionCommand, ApiResponse<ConnectionResponse>>,
+    IRequestHandler<DeleteConnectionCommand, ApiResponse<ConnectionResponse>>
 {
-    private readonly BackendDbContext _dbContext = dbContext;
-    private readonly IMapper _mapper = mapper;
-    private readonly IHandlerValidator _validate = validate;
-    private readonly IVaultService _vaultService = vaultService;
-    private readonly IUserService _userService = userService;
-    private readonly IDapperService _dapperService = dapperService;
 
+    public async Task<ApiResponse<ConnectionResponse>> Handle(TestConnectionCommand request,
+        CancellationToken cancellationToken)
+    {
+        DapperService connection = dapperService.Create(request.ConnectionString, request.DatabaseType);
+      
+        bool result = await connection.TestConnection(cancellationToken);
+        
+        if (result)
+        {
+            return new ApiResponse<ConnectionResponse>("Connected established successfully");
+        }
+        else
+        {
+            return new ApiResponse<ConnectionResponse>("Failed to establish connection");
+        }
+        
+    }
 
-    public async Task<ApiResponse<ConnectionResponse>> Handle(TestConnectionCommand request, CancellationToken cancellationToken)
+    public async Task<ApiResponse<ConnectionResponse>> Handle(CreateUrlConnectionCommand request,
+        CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
     }
 
-    public async Task<ApiResponse<ConnectionResponse>> Handle(CreateUrlConnectionCommand request, CancellationToken cancellationToken)
+    public async Task<ApiResponse<ConnectionResponse>> Handle(CreateHostConnectionCommand request,
+        CancellationToken cancellationToken)
     {
         /*
          * check null and enum
          * User can only create connection for himself
          * create unique db id and use it for fluentvalidation also
          */
-
+        int userId = userService.GetUserId();
         string vaultIdentifier = Guid.NewGuid().ToString();
-        
-        
-        // create connection string
+        DatabaseType databaseType = Enum.Parse<DatabaseType>(request.Model.DatabaseType);
+        //create connection string
+        string connectionString = CreateConnectionStringFromUrl(model: request.Model, databaseType: databaseType);
+
         // test connection
+        var result = await mediator.Send(new TestConnectionCommand(connectionString, databaseType), cancellationToken);
+        
+        if (result.Success == false)
+        {
+            throw new HttpException(result.Message, 400);
+        }
+        
+        // save vault
+        await vaultService.SaveOrUpdateCredentials("users/" + userId + "/databases/" + vaultIdentifier,connectionString);
+        
+        var entity = mapper.Map<Connection>(request.Model);
+        entity.VaultIdentifier = vaultIdentifier;
+        entity.UserId = userId;
+        
+        // save db
+        await dbContext.Connections.AddAsync(entity, cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
         // if success save once vault sonra db
         // if false save db
+
+        // başarılı olursa  connected olmazsa kaydetme hiçbir yere
         
-        // başarılı olursa  connected olmazsa failed olur
-        
-        string connectionString = _vaultService.CreateConnectionString(request.Model);
-        _dapperService.Create()
+        var response = mapper.Map<ConnectionResponse>(entity);
 
-        // var entity = new Connection
-        // {
-        //     VaultIdentifier = VaultIdentifier,
-        //     ConnectionType = ConnectionType.Url,
-        //     UserId = userService.GetUserId(),
-        //     DatabaseName = request.Model.DatabaseName,
-        //     DatabaseType = request.Model.DatabaseType,
-        //     Status = ConnectionStatus.Connected,
-        //     CreationDate = DateTime.Now,
-        //     LastUpdateTime = DateTime.Now,
-        // };
-
-        return new ApiResponse<ConnectionResponse>(new ConnectionResponse() { Status = "Connected" });
+        return new ApiResponse<ConnectionResponse>(response);
     }
 
-    public async Task<ApiResponse<ConnectionResponse>> Handle(CreateHostConnectionCommand request, CancellationToken cancellationToken)
+    public async Task<ApiResponse<ConnectionResponse>> Handle(UpdateUrlConnectionCommand request,
+        CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
     }
 
-    public async Task<ApiResponse<ConnectionResponse>> Handle(UpdateUrlConnectionCommand request, CancellationToken cancellationToken)
+    public async Task<ApiResponse<ConnectionResponse>> Handle(UpdateHostConnectionCommand request,
+        CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
     }
 
-    public async Task<ApiResponse<ConnectionResponse>> Handle(UpdateHostConnectionCommand request, CancellationToken cancellationToken)
+    public async Task<ApiResponse<ConnectionResponse>> Handle(DeleteConnectionCommand request,
+        CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
     }
 
-    public async Task<ApiResponse<ConnectionResponse>> Handle(DeleteConnectionCommand request, CancellationToken cancellationToken)
+    /// <summary>
+    /// Connection Timeout means the time to wait (in seconds) while trying to establish a connection before terminating the attempt and generating an error. Connection Lifetime means the time (in seconds) to wait before closing idle connections.
+    /// </summary>
+    private string CreateConnectionStringFromUrl(CreateHostConnectionRequest model, DatabaseType databaseType)
     {
-        throw new NotImplementedException();
-    }
-
-    private string CreateConnectionStringFromUrl()
-    {
-        string skeletalConnectionString = "Server={0};Database={1};User Id={2};Password={3};{4}";
-        string poolingParameter = "Pooling=true;";
-        timeout = "Connection Timeout=30;";
-    }
-    
-    private static CreateConnectionStringFromHost()
-    {
-        
+        return databaseType switch
+        {
+            DatabaseType.SqlServer =>
+                $"Server={model.Host},{model.Port};Database={model.DatabaseOrSchema};User Id={model.Username};Password={model.Password};Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Timeout=5;Connection Lifetime=180;",
+            DatabaseType.MySql =>
+                $"Server={model.Host};Port={model.Port};Database={model.DatabaseOrSchema};Uid={model.Username};Pwd={model.Password};Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Timeout=5;Connection Lifetime=180;",
+            DatabaseType.PostgreSql =>
+                $"User ID={model.Username};Password={model.Password};Host={model.Host};Port={model.Port};Database={model.DatabaseOrSchema};Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Lifetime=0;",
+            DatabaseType.Oracle =>
+                // $"User ID={model.Username};Password={model.Password};Host={model.Host};Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Lifetime=0;",
+                $"Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST={model.Host})(PORT={model.Port})))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME={model.DatabaseOrSchema})));User Id={model.Username};Password={model.Password};Pooling=True;Connection Timeout=5;Connection Lifetime=180;",
+            _ => throw new ArgumentOutOfRangeException(nameof(databaseType), databaseType, null)
+        };
     }
 }
 
