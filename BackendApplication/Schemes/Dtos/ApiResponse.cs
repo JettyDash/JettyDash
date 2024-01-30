178a92a0294f949549f893e239d6ae29dd0213a9
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-30 21:54:06 +0300

    refactor:BackendApplication/Schemes/Dtos/ApiResponse.cs

--- BackendApplication/Schemes/Dtos/ApiResponse.cs
+++ BackendApplication/Schemes/Dtos/ApiResponse.cs
@@ -2,13 +2,24 @@ using System.Text.Json;
 
 namespace Schemes.Dtos;
 
-public class ApiResponse<T>(T content)
+public class ApiResponse<T>
 {
     public DateTime ServerDate { get; set; } = DateTime.UtcNow;
     public Guid ReferenceNo { get; set; } = Guid.NewGuid();
     public bool Success { get; set; } = true;
     public string Message { get; set; } = "Success";
-    public T Content { get; set; } = content;
-
+    public T? Content { get; set; }
+    
+    public ApiResponse(T content)
+    {
+        Content = content;
+    }
+    
+    public ApiResponse(string message)
+    {
+        Success = false;
+        Message = message;
+    }
+    
     public override string ToString() => JsonSerializer.Serialize(this);
 }
