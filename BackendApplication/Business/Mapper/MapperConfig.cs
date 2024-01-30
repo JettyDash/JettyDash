244f5eb9a4fa3be51bed30e5c2f9232720bd8d06
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-30 21:54:06 +0300

    refactor:BackendApplication/Business/Mapper/MapperConfig.cs

--- BackendApplication/Business/Mapper/MapperConfig.cs
+++ BackendApplication/Business/Mapper/MapperConfig.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Infrastructure.Entities;
 using Schemes.Dtos;
+using Schemes.Enums;
 
 namespace Business.Mapper;
 
@@ -8,11 +9,17 @@ public class MapperConfig : Profile
 {
     public MapperConfig()
     {
+        CreateMap<CreateHostConnectionRequest, Connection>()
+            .ForMember(databaseName => databaseName.DatabaseName, opt => opt.MapFrom(src => src.DatabaseName))
+            .ForMember(databaseType => databaseType.DatabaseType, opt => opt.MapFrom(src => src.DatabaseType))
+            .ForMember(dest => dest.CreationDate, opt => opt.MapFrom(src => DateTime.Now))
+            .ForMember(dest => dest.LastUpdateTime, opt => opt.MapFrom(src => DateTime.Now))
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => ConnectionStatus.Connected))
+            .ForMember(dest => dest.ConnectionType, opt => opt.MapFrom(src => ConnectionType.Host));
     
         CreateMap<Connection, ConnectionResponse>();
         
         // CreateMap<CreateUrlConnectionRequest, Connection>();
-        // CreateMap<CreateHostConnectionRequest, Connection>();
         // CreateMap<UpdateUrlConnectionRequest, Connection>();
         // CreateMap<UpdateHostConnectionRequest, Connection>();
         
