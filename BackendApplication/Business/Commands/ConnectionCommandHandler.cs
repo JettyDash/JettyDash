ee347fe18247ba21e901edbfe93b799aba838165
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-31 21:18:59 +0300

    CreateConnectionStringFromUrl CreateCredentialDictionary CreateHostConnectionCommand TestConnectionCommand

--- BackendApplication/Business/Commands/ConnectionCommandHandler.cs
+++ BackendApplication/Business/Commands/ConnectionCommandHandler.cs
@@ -5,13 +5,16 @@ using Business.Validators;
 using Infrastructure.DbContext;
 using Infrastructure.Entities;
 using MediatR;
+using Microsoft.Extensions.Options;
 using Schemes.Dtos;
 using Schemes.Enums;
 using Schemes.Exceptions;
+using Schemes.Vault;
 
 namespace Business.Commands;
 
 public class ConnectionCommandHandler(
+    IOptions<VaultConfig> vaultConfig,
     BackendDbContext dbContext,
     IMapper mapper,
     IHandlerValidator validate,
@@ -26,23 +29,14 @@ public class ConnectionCommandHandler(
     IRequestHandler<UpdateHostConnectionCommand, ApiResponse<ConnectionResponse>>,
     IRequestHandler<DeleteConnectionCommand, ApiResponse<ConnectionResponse>>
 {
-
     public async Task<ApiResponse<ConnectionResponse>> Handle(TestConnectionCommand request,
         CancellationToken cancellationToken)
     {
         var connection = dapperServiceFactory.Create(request.ConnectionString, request.DatabaseType);
-      
-        bool result = await connection.TestConnection(cancellationToken);
-        
-        if (result)
-        {
-            return new ApiResponse<ConnectionResponse>("Connected established successfully");
-        }
-        else
-        {
-            return new ApiResponse<ConnectionResponse>("Failed to establish connection");
-        }
-        
+
+        (bool result, string message) = await connection.TestConnection(cancellationToken);
+
+        return new ApiResponse<ConnectionResponse>(message: message, success: result);
     }
 
     public async Task<ApiResponse<ConnectionResponse>> Handle(CreateUrlConnectionCommand request,
@@ -60,26 +54,36 @@ public class ConnectionCommandHandler(
          * create unique db id and use it for fluentvalidation also
          */
         int userId = userService.GetUserId();
-        string vaultIdentifier = Guid.NewGuid().ToString();
+        string vaultIdentifier = Guid.NewGuid().ToString().ToUpper();
         DatabaseType databaseType = Enum.Parse<DatabaseType>(request.Model.DatabaseType);
+
+
         //create connection string
         string connectionString = CreateConnectionStringFromUrl(model: request.Model, databaseType: databaseType);
 
         // test connection
-        var result = await mediator.Send(new TestConnectionCommand(connectionString, databaseType), cancellationToken);
-        
+
+        var testConnectionCommand = new TestConnectionCommand(connectionString, databaseType);
+        var result = await mediator.Send(testConnectionCommand, cancellationToken);
+
         if (result.Success == false)
         {
             throw new HttpException(result.Message, 400);
         }
-        
+
         // save vault
-        await vaultService.SaveOrUpdateCredentials("users/" + userId + "/databases/" + vaultIdentifier,connectionString);
-        
+        // "users/" + userId + "/databases/" + vaultIdentifier
+
+        await vaultService.SaveOrUpdateCredentials(
+            path: vaultConfig.Value.DatabaseSecretsPath,
+            values: CreateCredentialDictionary(userId, vaultIdentifier, connectionString),
+            mountPoint: vaultConfig.Value.Mount
+        );
+
         var entity = mapper.Map<Connection>(request.Model);
         entity.VaultIdentifier = vaultIdentifier;
         entity.UserId = userId;
-        
+
         // save db
         await dbContext.Connections.AddAsync(entity, cancellationToken);
         await dbContext.SaveChangesAsync(cancellationToken);
@@ -88,7 +92,7 @@ public class ConnectionCommandHandler(
         // if false save db
 
         // başarılı olursa  connected olmazsa kaydetme hiçbir yere
-        
+
         var response = mapper.Map<ConnectionResponse>(entity);
 
         return new ApiResponse<ConnectionResponse>(response);
@@ -119,17 +123,31 @@ public class ConnectionCommandHandler(
     {
         return databaseType switch
         {
+            // Only SQL Authentication mode is supported
+            // TODO: trıust server certificate true to false
             DatabaseType.SqlServer =>
-                $"Server={model.Host},{model.Port};Database={model.DatabaseOrSchema};User Id={model.Username};Password={model.Password};Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Timeout=5;Connection Lifetime=180;",
+                $"Server={model.Host},{model.Port};Database={model.DatabaseName};User Id={model.Username};Password={model.Password};Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Timeout=5;Connection Lifetime=180;" +
+                "Integrated Security=false;Encrypt=true;TrustServerCertificate=true;MultipleActiveResultSets=true;",
+
             DatabaseType.MySql =>
-                $"Server={model.Host};Port={model.Port};Database={model.DatabaseOrSchema};Uid={model.Username};Pwd={model.Password};Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Timeout=5;Connection Lifetime=180;",
+                $"Server={model.Host};Port={model.Port};Database={model.DatabaseName};Uid={model.Username};Pwd={model.Password};Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Timeout=5;Connection Lifetime=180;",
             DatabaseType.PostgreSql =>
-                $"User ID={model.Username};Password={model.Password};Host={model.Host};Port={model.Port};Database={model.DatabaseOrSchema};Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Lifetime=0;",
+                $"User ID={model.Username};Password={model.Password};Host={model.Host};Port={model.Port};Database={model.DatabaseName};Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Lifetime=0;",
             DatabaseType.Oracle =>
                 // $"User ID={model.Username};Password={model.Password};Host={model.Host};Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Lifetime=0;",
-                $"Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST={model.Host})(PORT={model.Port})))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME={model.DatabaseOrSchema})));User Id={model.Username};Password={model.Password};Pooling=True;Connection Timeout=5;Connection Lifetime=180;",
+                $"Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST={model.Host})(PORT={model.Port})))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME={model.DatabaseName})));User Id={model.Username};Password={model.Password};Pooling=True;Connection Timeout=5;Connection Lifetime=180;",
             _ => throw new ArgumentOutOfRangeException(nameof(databaseType), databaseType, null)
         };
+    }
+
+
+    private Dictionary<string, string> CreateCredentialDictionary(int userId, string vaultIdentifier,
+        string connectionString)
+    {
+        string credentialKey = string.Format(Constants.VaultPath.Database, userId, vaultIdentifier);
+        Dictionary<string, string> values = new() { { credentialKey, connectionString } };
+
+        return values;
     }
 }
 
