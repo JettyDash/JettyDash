9388bc69b59947326d30d2ed7833ad2ed5fbc166
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-31 21:14:29 +0300

    added two new fields

--- BackendApplication/Schemes/Vault/VaultConfig.cs
+++ BackendApplication/Schemes/Vault/VaultConfig.cs
@@ -2,8 +2,9 @@ namespace Schemes.Vault;
 
 public class VaultConfig
 {
-    public string Address { get; set; }
+    public string Address { get; set;}
     public string Token { get; set; }
     public int EngineVersion { get; set; }
-    
+    public string DatabaseSecretsPath { get; set; }
+    public string Mount { get; set; }
 }
