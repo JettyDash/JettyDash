67d060eaef2a2712646895ea53f0cc82c996e445
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-01 02:19:47 +0300

    pipeline behavior:BackendApplication/Business/Preprocessor/ConnectionPreprocessor.cs

--- BackendApplication/Business/Preprocessor/ConnectionPreprocessor.cs
+++ BackendApplication/Business/Preprocessor/ConnectionPreprocessor.cs
@@ -1,32 +1,45 @@
+using Business.Cqrs;
+using MediatR;
+
+namespace Business.Preprocessor;
+
+public class CreateHostConnectionCommandPreprocessor1<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : CreateHostConnectionCommand
+
+{
+    // TODO: LEFT https://www.jimmybogard.com/sharing-context-in-mediatr-pipelines/
+
+    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        Console.WriteLine("CreateHostConnectionCommandPreprocessor1 worked)");
+        return next();
+
+    }
+}
+
+
+public class CreateHostConnectionCommandPreprocessor2<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : CreateHostConnectionCommand
+
+{
+    
+
+    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        Console.WriteLine("CreateHostConnectionCommandPreprocessor2 worked)");
+        return next();
+
+    }
+}
+
+
+// public class CreateHostConnectionCommandPreprocessor3<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
 //
-// using MediatR;
-// using System;
-// using System.Threading;
-// using System.Threading.Tasks;
-// using MediatR.Pipeline;
-// using Schemes.Dtos;
-// using Schemes.Enums;
-//
-// namespace Business.Preprocessor;
-//
-// public class CreateUrlConnectionRequestPreProcessor : IRequestPreProcessor<CreateUrlConnectionRequest>
 // {
-//     public Task Process(CreateUrlConnectionRequest request, CancellationToken cancellationToken)
-//     {
-//         // Convert string DatabaseType to Enum
-//         if (!string.IsNullOrEmpty(request.DatabaseType))
-//         {
-//             if (Enum.TryParse(request.DatabaseType, out DatabaseType databaseType))
-//             {
-//                 request.DatabaseType = databaseType;
-//             }
-//             else
-//             {
-//                 // Handle invalid DatabaseType here if needed
-//                 throw new InvalidOperationException("Invalid DatabaseType");
-//             }
-//         }
+//     
 //
-//         return Task.CompletedTask;
+//     public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+//     {
+//         Console.WriteLine("CreateHostConnectionCommandPreprocessor2 worked)");
+//         return next();
+//
 //     }
 // }
