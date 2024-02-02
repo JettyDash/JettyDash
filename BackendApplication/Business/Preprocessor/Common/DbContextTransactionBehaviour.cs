68162727e841c9fda25963e5c6a02bbdd474c149
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-02 18:57:25 +0300

    transaction commit fix:BackendApplication/Business/Preprocessor/Common/DbContextTransactionBehaviour.cs

--- BackendApplication/Business/Preprocessor/Common/DbContextTransactionBehaviour.cs
+++ BackendApplication/Business/Preprocessor/Common/DbContextTransactionBehaviour.cs
@@ -1,18 +1,36 @@
 using Business.Cqrs;
+using Infrastructure.DbContext;
 using MediatR;
+using Schemes.Exceptions;
 using Schemes.Mediatr;
 
 namespace Business.Preprocessor.Common;
 
-public class DbContextTransactionBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : ICommand<TResponse>
+public class DbContextTransactionBehaviour<TRequest, TResponse>(BackendDbContext dbContext)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : ICommand<TResponse>
 {
     // TODO: Only commands must be transactional
-    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
         CancellationToken cancellationToken)
     {
         if (request is not CreateTokenCommand)
         {
+            await using var transaction = dbContext.Database.CurrentTransaction ?? 
+                                          await dbContext.Database.BeginTransactionAsync(cancellationToken);
+            try
+            {
+                var response = await next();
+                await transaction.CommitAsync(cancellationToken);
+                return response;
+            }
+            catch (Exception e)
+            {
+                await transaction.RollbackAsync(cancellationToken);
+                throw new HttpException($"Transaction failed: {e.Message}", 500);
+            }
         }
-        return next();
+        
+        return await next();
     }
 }
