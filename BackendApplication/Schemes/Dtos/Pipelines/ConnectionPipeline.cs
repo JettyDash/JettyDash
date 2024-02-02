73e8b4fac42947e79df0418094f2240c186c5a8f
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-02 15:47:01 +0300

    pipeline dtos added

---
+++ BackendApplication/Schemes/Dtos/Pipelines/ConnectionPipeline.cs
@@ -0,0 +1,29 @@
+using Schemes.Enums;
+
+namespace Schemes.Dtos.Pipelines;
+
+public interface IContextualDto
+{
+    int UserId { get; set; }
+    string UserRole { get; set; }
+}
+
+
+public class ConnectionPipeline(string userRole, int userId) : IContextualDto
+{
+    public int UserId { get; set; } = userId;
+    public string UserRole { get; set; } = userRole;
+}
+
+public class CreateHostConnectionPipeline(string userRole, int userId) : ConnectionPipeline(userRole, userId)
+{
+    public string ConnectionString { get; set; } = string.Empty;
+    public DatabaseType DatabaseType { get; set; } = DatabaseType.Unknown;
+    public string VaultIdentifier { get; set; } = string.Empty;
+}
+
+
+
+public class GetAllConnectionPipeline(string userRole, int userId) : ConnectionPipeline(userRole, userId)
+{
+}
