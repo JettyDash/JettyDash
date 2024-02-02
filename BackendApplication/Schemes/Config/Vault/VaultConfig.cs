84d4e5abc00261677401aadbdd7654b1acefc1cd
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-03 00:16:16 +0300

    vault path fixes:BackendApplication/Schemes/Config/Vault/VaultConfig.cs

--- BackendApplication/Schemes/Config/Vault/VaultConfig.cs
+++ BackendApplication/Schemes/Config/Vault/VaultConfig.cs
@@ -5,6 +5,5 @@ public class VaultConfig
     public string Address { get; set;}
     public string Token { get; set; }
     public int EngineVersion { get; set; }
-    public string DatabaseSecretsPath { get; set; }
     public string Mount { get; set; }
 }
