a47b89c0216e1d2b447d22c3f763900bd93ac988
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-03 18:04:07 +0300

    Added Vault Secret Engine

--- BackendApplication/Business/Preprocessor/Common/DbContextTransactionBehaviour.cs
+++ BackendApplication/Business/Preprocessor/Common/DbContextTransactionBehaviour.cs
@@ -13,7 +13,7 @@ public class DbContextTransactionBehaviour<TRequest, TResponse>(BackendDbContext
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
         CancellationToken cancellationToken)
     {
-        if (request is not CreateTokenCommand)
+        if (request is not (CreateTokenCommand or TestConnectionCommand))
         {
             await using var transaction = dbContext.Database.CurrentTransaction ?? 
                                           await dbContext.Database.BeginTransactionAsync(cancellationToken);
