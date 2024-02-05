19047001dec3d8729560c1bd6129373f7e83ba53
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-05 15:49:12 +0300

    https support added and readme files:BackendApplication/Api/Program.cs

--- BackendApplication/Api/Program.cs
+++ BackendApplication/Api/Program.cs
@@ -1,13 +1,38 @@
-namespace Api;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 
-public abstract class Program
+namespace Api
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    ConfigureKestrel(webBuilder);
+                    webBuilder.UseStartup<Startup>();
+                });
+
+        private static void ConfigureKestrel(IWebHostBuilder webBuilder)
+        {
+            webBuilder.UseKestrel((context, options) =>
             {
-                webBuilder.UseStartup<Startup>();
-            }).Build().Run();
+                var endpoints = context.Configuration.GetSection("Kestrel:Endpoints");
+                var certPath = endpoints.GetValue<string>("Https:Certificate:Path");
+                var keyPath = endpoints.GetValue<string>("Https:Certificate:Password");
+                var certificate = new X509Certificate2(certPath, keyPath);
+
+                options.Listen(IPAddress.Loopback, 5268); // HTTP
+                options.Listen(IPAddress.Loopback, 7268, listenOptions =>
+                {
+                    listenOptions.UseHttps(certificate); // HTTPS
+                });
+            });
+        }
     }
 }
