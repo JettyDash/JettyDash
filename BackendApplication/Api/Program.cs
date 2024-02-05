5e8a71cff34cee7f8a87d28ccf2bd1b5f12edc88
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-05 18:22:58 +0300

    removed https support binding error and permission denied:BackendApplication/Api/Program.cs

--- BackendApplication/Api/Program.cs
+++ BackendApplication/Api/Program.cs
@@ -14,25 +14,9 @@ namespace Api
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    ConfigureKestrel(webBuilder);
                     webBuilder.UseStartup<Startup>();
+                    
                 });
 
-        private static void ConfigureKestrel(IWebHostBuilder webBuilder)
-        {
-            webBuilder.UseKestrel((context, options) =>
-            {
-                var endpoints = context.Configuration.GetSection("Kestrel:Endpoints");
-                var certPath = endpoints.GetValue<string>("Https:Certificate:Path");
-                var keyPath = endpoints.GetValue<string>("Https:Certificate:Password");
-                var certificate = new X509Certificate2(certPath, keyPath);
-
-                options.Listen(IPAddress.Loopback, 5268); // HTTP
-                options.Listen(IPAddress.Loopback, 7268, listenOptions =>
-                {
-                    listenOptions.UseHttps(certificate); // HTTPS
-                });
-            });
-        }
     }
 }
