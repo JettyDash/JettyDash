581a4d4d22a278476dc11d80c26e9063e1007c74
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-21 06:12:55 +0300

    pages added:WebsiteApplication/client/app/(root)/settings/page.tsx

--- WebsiteApplication/client/app/(root)/settings/page.tsx
+++ WebsiteApplication/client/app/(root)/settings/page.tsx
@@ -3,20 +3,13 @@ import Link from "next/link";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Home | JettyDash",
+  title: "Settings | JettyDash",
 };
 
 export default async function Home({ }) {
-  // const { userId } = auth();
-  const userId = "1";
-
-  let result;
-
   return (
     <div>
-      <h1>Connections</h1>
-      <p>Welcome to JettyDash</p>
-      <Link href={"google.com"}>Posts</Link>
+      <h1>Settings</h1>
     </div>
   );
 }
