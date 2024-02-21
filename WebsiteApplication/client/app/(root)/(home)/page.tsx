8fdfdb774abdf8606235754770f2e46530811fcc
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-21 08:47:35 +0300

    set active tab:WebsiteApplication/client/app/(root)/(home)/page.tsx

--- WebsiteApplication/client/app/(root)/(home)/page.tsx
+++ WebsiteApplication/client/app/(root)/(home)/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Home | Dev Overflow",
+  title: "JettyDash",
 };
 
 export default async function Home({ }) {
