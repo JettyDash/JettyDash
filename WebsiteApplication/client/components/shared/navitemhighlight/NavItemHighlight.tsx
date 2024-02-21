f98c4b5785dcafd4572c75864530812f7bc032f2
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-21 05:19:58 +0300

    navbar and cleaning:WebsiteApplication/client/components/shared/navitemhighlight/NavItemHighlight.tsx

--- WebsiteApplication/client/components/shared/navitemhighlight/NavItemHighlight.tsx
+++ WebsiteApplication/client/components/shared/navitemhighlight/NavItemHighlight.tsx
@@ -1,14 +1,12 @@
-import styles from './styles.module.scss'
-
 interface NavItemHighlightProps {
     width: number;
     translateX: number;
 }
 
-export function NavItemHighlight ({ width, translateX }: NavItemHighlightProps) {
+export function NavItemHighlight({ width, translateX }: NavItemHighlightProps) {
     return (
         <div
-            className={styles.container}
+            className="absolute h-8 bg-gray-200 dark:bg-neutral-700 z-0 rounded-md transform duration-300 ease-in-out"
             style={{
                 width: `${width}px`,
                 transform: `translateX(${translateX}px)`
@@ -16,5 +14,3 @@ export function NavItemHighlight ({ width, translateX }: NavItemHighlightProps) 
         />
     )
 }
-
-// 20.7
