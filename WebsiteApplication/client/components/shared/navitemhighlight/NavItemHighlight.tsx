73e97363572d97328c3cb693ecda155026ac5303
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-21 06:12:55 +0300

    pages added:WebsiteApplication/client/components/shared/navitemhighlight/NavItemHighlight.tsx

--- WebsiteApplication/client/components/shared/navitemhighlight/NavItemHighlight.tsx
+++ WebsiteApplication/client/components/shared/navitemhighlight/NavItemHighlight.tsx
@@ -3,10 +3,10 @@ interface NavItemHighlightProps {
     translateX: number;
 }
 
-export function NavItemHighlight({ width, translateX }: NavItemHighlightProps) {
+export function NavItemHighlight({width, translateX }: NavItemHighlightProps) {
     return (
         <div
-            className="absolute h-8 bg-gray-200 dark:bg-neutral-700 z-0 rounded-md transform duration-300 ease-in-out"
+            className="absolute h-8 bg-gray-200 dark:bg-neutral-700 z-0 rounded-md transition-all duration-300 ease-in-out"
             style={{
                 width: `${width}px`,
                 transform: `translateX(${translateX}px)`
