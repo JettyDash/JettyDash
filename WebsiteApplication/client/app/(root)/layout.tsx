dfe04c79e6f5adc38998a37dee607a822b565579
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-22 14:19:41 +0300

    connections page and flicker fix:WebsiteApplication/client/app/(root)/layout.tsx

--- WebsiteApplication/client/app/(root)/layout.tsx
+++ WebsiteApplication/client/app/(root)/layout.tsx
@@ -6,16 +6,14 @@ import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <main className=" background-light850_dark100 relative">
+        // background-light850_dark100
+        <main className="relative">
             <Navbar />
-            <div className="flex">
-                {/*<LeftSidebar />*/}
-                <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
-                    <div className="mx-auto w-full">{children}</div>
-                </section>
-                {/*<RightSidebar />*/}
+            <div>
+                <div className="flex w-full h-full flex-1 flex-col px-6 pb-6 pt-10 max-md:pb-14 sm:px-14">
+                    <section className="mx-auto w-full">{children}</section>
+                </div>
             </div>
-            {/*<Toaster />*/}
         </main>
     );
 };
