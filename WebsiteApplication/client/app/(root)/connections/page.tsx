8a67b469a3f1c858000165f76ea22bdf52a85dba
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-23 05:30:47 +0300

    MySql Icon Added:WebsiteApplication/client/app/(root)/connections/page.tsx

--- WebsiteApplication/client/app/(root)/connections/page.tsx
+++ WebsiteApplication/client/app/(root)/connections/page.tsx
@@ -3,9 +3,16 @@ import Link from "next/link";
 import type {Metadata} from "next";
 import {Divider} from "@nextui-org/divider";
 import UserTable from "@/components/ui/usertable";
+import PostgresDbIcon from "@/components/icons/PostgresDbIcon";
+import MySqlDbIcon from "@/components/icons/MySqlDbIcon";
 
 export const metadata: Metadata = {
-    title: "Connections | JettyDash",
+    title: "JettyDash",
+    description:
+        "JettyDash is a modern, open-source dashboard creator for your business.",
+    icons: {
+        icon: "/assets/images/site-logo.svg",
+    },
 };
 
 export default async function Connections({}) {
@@ -21,9 +28,9 @@ export default async function Connections({}) {
                             databases without storing any information.
                         </p>
                         <div className="learnmore relative inline-flex group items-center">
-                            <a href="docs" className="inline-flex relative text-[#3291FF] body-regular text-start font-ubuntu">Learn More</a>
+                            <a href="/docs" className="inline-flex relative text-[#3291FF] body-regular text-start font-ubuntu">Learn More</a>
                             <svg className="w-4 h-4 inline-flex ml-1 relative text-[#3291FF]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                       d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"/>
                             </svg>
                             <div className="svg_underline absolute bottom-0 left-0 pointer-events-none bg-transparent transition-all"></div>
@@ -36,10 +43,19 @@ export default async function Connections({}) {
                 </div>
                 <Divider orientation="horizontal" className="my-10 dark-300"/>
 
-                <div className="border border-solid border-light-300 bg-stone-100 shadow-md rounded-xl px-10 py-5 dark:border-none dark:bg-transparent">
+                <div className="border border-solid border-light-300 bg-stone-100 shadow-md rounded-xl px-10 py-5 dark:border-dark-300 dark:bg-transparent">
                     <UserTable/>
                 </div>
 
+                <div>
+                    <PostgresDbIcon/>
+                    <MySqlDbIcon/>
+
+
+
+                </div>
+                
+                
 
             </div>
             <div className="flex-1"></div>
