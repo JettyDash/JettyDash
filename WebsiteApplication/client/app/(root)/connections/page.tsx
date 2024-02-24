3b03853a9d316a6ed4b69a475250c207bf4cacfe
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-24 06:36:33 +0300

    commit:WebsiteApplication/client/app/(root)/connections/page.tsx

--- WebsiteApplication/client/app/(root)/connections/page.tsx
+++ WebsiteApplication/client/app/(root)/connections/page.tsx
@@ -1,11 +1,12 @@
-import Link from "next/link";
-
 import type {Metadata} from "next";
 import {Divider} from "@nextui-org/divider";
 import UserTable from "@/components/ui/usertable";
 import PostgresDbIcon from "@/components/icons/PostgresDbIcon";
 import MySqlDbIcon from "@/components/icons/MySqlDbIcon";
 import OracleDbIcon from "@/components/icons/OracleDbIcon";
+import React from "react";
+import MsSqlDbIcon from "@/components/icons/MsSqlDbIcon";
+
 
 export const metadata: Metadata = {
     title: "JettyDash",
@@ -25,26 +26,32 @@ export default async function Connections({}) {
                     <div>
                         <h1 className="h1-bold font-ubuntu text-dark-200 dark:text-stone-300">Create
                             Connection</h1>
-                        <p className="body-regular relative inline-flex font-ubuntu text-dark-500 dark:text-stone-400 mt-3 mr-1">Read directly from
+                        <p className="body-regular relative inline-flex font-ubuntu text-dark-500 dark:text-stone-400 mt-3 mr-1">Read
+                            directly from
                             databases without storing any information.
                         </p>
                         <div className="learnmore relative inline-flex group items-center">
-                            <a href="/docs" className="inline-flex relative text-[#3291FF] body-regular text-start font-ubuntu">Learn More</a>
-                            <svg className="w-4 h-4 inline-flex ml-1 relative text-[#3291FF]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <a href="/docs"
+                               className="inline-flex relative text-[#3291FF] body-regular text-start font-ubuntu">Learn
+                                More</a>
+                            <svg className="w-4 h-4 inline-flex ml-1 relative text-[#3291FF]"
+                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                       d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"/>
                             </svg>
-                            <div className="svg_underline absolute bottom-0 left-0 pointer-events-none bg-transparent transition-all"></div>
+                            <div
+                                className="svg_underline absolute bottom-0 left-0 pointer-events-none bg-transparent transition-all"></div>
                         </div>
 
-                        {/*    rgb(50, 145, 255) 14 px and a svg*/}
                     </div>
 
 
                 </div>
                 <Divider orientation="horizontal" className="my-10 dark-300"/>
 
-                <div className="border border-solid border-light-300 bg-stone-100 shadow-md rounded-xl px-10 py-5 dark:border-dark-300 dark:bg-transparent">
+                <div
+                    className="border border-solid border-light-300 bg-stone-100 shadow-md rounded-xl px-10 py-5 dark:border-dark-300 dark:bg-transparent">
                     <UserTable/>
                 </div>
 
@@ -52,17 +59,12 @@ export default async function Connections({}) {
                     <PostgresDbIcon/>
                     <MySqlDbIcon/>
                     <OracleDbIcon/>
-
+                    <MsSqlDbIcon/>
 
 
                 </div>
-                
-                
-
             </div>
             <div className="flex-1"></div>
         </div>
-
-
     );
 }
