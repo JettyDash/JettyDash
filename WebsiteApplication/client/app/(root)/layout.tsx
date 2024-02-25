3ea62a27c39bec3046ef9f7b4ad430e244e9f7d1
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-25 17:19:41 +0300

    date column:WebsiteApplication/client/app/(root)/layout.tsx

--- WebsiteApplication/client/app/(root)/layout.tsx
+++ WebsiteApplication/client/app/(root)/layout.tsx
@@ -1,12 +1,8 @@
-// import LeftSidebar from "@/components/shared/LeftSidebar";
-// import RightSidebar from "@/components/shared/RightSidebar";
 import Navbar from "@/components/shared/navbar/Navbar";
-// import { Toaster } from "@/components/ui/toaster";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        // background-light850_dark100
         <main className="relative">
             <Navbar />
             <div>
