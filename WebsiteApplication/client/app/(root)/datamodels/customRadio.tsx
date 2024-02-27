4478671a59f410a820d0b69d98c8320c81dd77b3
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-27 19:13:20 +0300

    DatabaseCards component added:WebsiteApplication/client/app/(root)/datamodels/customRadio.tsx

--- WebsiteApplication/client/app/(root)/datamodels/customRadio.tsx
+++ WebsiteApplication/client/app/(root)/datamodels/customRadio.tsx
@@ -20,17 +20,15 @@ export const CustomRadio = (props: RadioProps) => {
         <Component
             {...getBaseProps()}
             className={cn(
-                "group inline-flex items-center justify-between hover:bg-content2 flex-row-reverse",
-                "max-w-[300px] cursor-pointer border-2 border-default rounded-lg gap-4 p-4",
+                "h-24 w-24 group hover:bg-content2 flex-row-reverse",
+                "max-w-[300px] cursor-pointer border-3 border-default rounded-lg gap-4 p-4",
                 "data-[selected=true]:border-primary",
             )}
         >
             <VisuallyHidden>
                 <input {...getInputProps()} />
             </VisuallyHidden>
-            <span {...getWrapperProps()}>
-        <span {...getControlProps()} />
-      </span>
+
             <div {...getLabelWrapperProps()}>
                 {children && <span {...getLabelProps()}>{children}</span>}
                 {description && (
