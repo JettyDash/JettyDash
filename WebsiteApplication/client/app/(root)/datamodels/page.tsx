13764e4cdefe6a477491bbe03644aad070ac538c
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-27 15:50:23 +0300

    theme fixes also radio groups working solution:WebsiteApplication/client/app/(root)/datamodels/page.tsx

--- WebsiteApplication/client/app/(root)/datamodels/page.tsx
+++ WebsiteApplication/client/app/(root)/datamodels/page.tsx
@@ -1,15 +1,39 @@
-import Link from "next/link";
+// import Link from "next/link";
+//
+// import type { Metadata } from "next";
+//
+// export const metadata: Metadata = {
+//   title: "DataModels | JettyDash",
+// };
+//
+// export default async function DataModels({ }) {
+//   return (
+//     <div>
+//       <h1>DataModels</h1>
+//     </div>
+//   );
+// }
 
-import type { Metadata } from "next";
+'use client';
+import React from "react";
+import {RadioGroup} from "@nextui-org/react";
+import {CustomRadio} from "@/app/(root)/datamodels/customRadio";
 
-export const metadata: Metadata = {
-  title: "DataModels | JettyDash",
-};
-
-export default async function DataModels({ }) {
-  return (
-    <div>
-      <h1>DataModels</h1>
-    </div>
-  );
+export default function DataModels() {
+    return (
+        <RadioGroup orientation="horizontal" label="Plans">
+            <CustomRadio description="Up to 20 items" value="free">
+                Free
+            </CustomRadio>
+            <CustomRadio description="Unlimited items. $10 per month." value="pro">
+                Pro
+            </CustomRadio>
+            <CustomRadio
+                description="24/7 support. Contact us for pricing."
+                value="enterprise"
+            >
+                Enterprise
+            </CustomRadio>
+        </RadioGroup>
+    );
 }
