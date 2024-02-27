56ed9b888d9451e396c7d69ad4522640b1f5cf08
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-27 19:13:20 +0300

    DatabaseCards component added:WebsiteApplication/client/app/(root)/datamodels/page.tsx

--- WebsiteApplication/client/app/(root)/datamodels/page.tsx
+++ WebsiteApplication/client/app/(root)/datamodels/page.tsx
@@ -16,24 +16,11 @@
 
 'use client';
 import React from "react";
-import {RadioGroup} from "@nextui-org/react";
-import {CustomRadio} from "@/app/(root)/datamodels/customRadio";
+import {DatabaseCards} from "@/components/connections/databasecards";
+
 
 export default function DataModels() {
     return (
-        <RadioGroup orientation="horizontal" label="Plans">
-            <CustomRadio description="Up to 20 items" value="free">
-                Free
-            </CustomRadio>
-            <CustomRadio description="Unlimited items. $10 per month." value="pro">
-                Pro
-            </CustomRadio>
-            <CustomRadio
-                description="24/7 support. Contact us for pricing."
-                value="enterprise"
-            >
-                Enterprise
-            </CustomRadio>
-        </RadioGroup>
+    <DatabaseCards />
     );
 }
