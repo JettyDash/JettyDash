ba4f42af3e3f1ed786def3bd106bf96af77557e0
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-28 14:44:31 +0300

    git push:WebsiteApplication/client/app/(root)/datamodels/page.tsx

--- WebsiteApplication/client/app/(root)/datamodels/page.tsx
+++ WebsiteApplication/client/app/(root)/datamodels/page.tsx
@@ -1,26 +1,60 @@
-// import Link from "next/link";
-//
-// import type { Metadata } from "next";
-//
-// export const metadata: Metadata = {
-//   title: "DataModels | JettyDash",
-// };
-//
-// export default async function DataModels({ }) {
-//   return (
-//     <div>
-//       <h1>DataModels</h1>
-//     </div>
-//   );
-// }
-
 'use client';
 import React from "react";
-import {DatabaseCards} from "@/components/connections/databasecards";
+import {
+    Modal,
+    ModalContent,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Button,
+    Checkbox,
+    Input,
+    Link,
+    Card, CardBody, Tabs, Tab, RadioGroup, Radio, useDisclosure
+} from "@nextui-org/react";
+import {PlusIcon} from "@/components/icons/PlusIcon";
+import {Key} from "@react-types/shared";
+
 
 
 export default function DataModels() {
+    const {isOpen, onOpen, onOpenChange} = useDisclosure();
+    const [selected, setSelected] = React.useState<Key>("login");
+
     return (
-        <DatabaseCards imageSizeClasses={"h-10 w-10"} baseCardClasses={"size-24"} />
+        <>
+            <Button
+                onPress={onOpen}
+                className="bg-foreground text-background"
+                endContent={<PlusIcon/>}
+                size="sm"
+            >
+                Add New
+            </Button>
+            <Modal
+                isOpen={isOpen}
+                onOpenChange={onOpenChange}
+                placement="bottom-center" // center on desktop, bottom on mobile
+            >
+                <ModalContent className="flex items-center w-[800px] h-[600px]">
+                    {(onClose) => (
+                        <>
+                            <ModalHeader className="flex flex-col gap-1">New Connection</ModalHeader>
+                            <ModalBody className="w-full h-full">
+
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button color="danger" variant="flat" onPress={onClose}>
+                                    Close
+                                </Button>
+                                <Button onPress={onClose}>
+                                    Sign in
+                                </Button>
+                            </ModalFooter>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
+        </>
     );
 }
