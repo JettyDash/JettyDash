4fb13c7973d30124ca27023619a2d3aa066373d3
Riza Semih Koca <rsemihkoca@outlook.com>
2024-02-28 14:44:31 +0300

    git push:WebsiteApplication/client/app/(root)/dashboards/page.tsx

--- WebsiteApplication/client/app/(root)/dashboards/page.tsx
+++ WebsiteApplication/client/app/(root)/dashboards/page.tsx
@@ -15,14 +15,13 @@
 //
 
 'use client';
-import {Tabs, Tab, Input, Link, Button, Card, CardBody, CardHeader} from "@nextui-org/react";
+import {Tabs, Tab, Input, Link, Button, Card, CardBody, CardHeader, CardFooter} from "@nextui-org/react";
 import React from "react";
 import {Key} from "@react-types/shared";
-import Image from "next/image";
 import {DatabaseCards} from "@/components/connections/databasecards";
 
 export default function Dashboards({}) {
-    const [selected, setSelected] = React.useState<Key>("login");
+    const [selected, setSelected] = React.useState<Key>("host");
     const [database, setDatabase] = React.useState<string>("")
     const [port, setPort] = React.useState<string>("1234"); // Add state for port number
 
@@ -48,14 +47,13 @@ export default function Dashboards({}) {
 
     return (
         <div className="flex flex-col w-full">
-            <Card className="max-w-full w-[440px] h-[700px]">
-                <CardHeader>
-                    <Image className={"dark:invert"} width={"32"} height={"32"} src={"../../../assets/icons/global.svg"}
-                           alt={"global"}/>
-                </CardHeader>
+            <Card className="max-w-full w-[440px] h-[560px]">
+                {/*<CardHeader>*/}
+                {/*    <Image className={"dark:invert"} width={"32"} height={"32"} src={"../../../assets/icons/global.svg"}*/}
+                {/*           alt={"global"}/>*/}
+                {/*</CardHeader>*/}
                 <CardBody className="overflow-hidden">
                     <Tabs
-
                         fullWidth
                         variant="bordered"
                         size="sm"
@@ -64,8 +62,8 @@ export default function Dashboards({}) {
                         onSelectionChange={setSelected}
                     >
 
-                        <Tab key="login" title="Login">
-                            <form className="flex flex-col gap-3">
+                        <Tab key="host" title="Host">
+                            <form className="flex flex-col gap-4">
                                 <DatabaseCards
                                     onDatabaseSelect={selectedDatabase => {
                                         setDatabase(selectedDatabase);
@@ -116,16 +114,16 @@ export default function Dashboards({}) {
                                     />
                                 </div>
 
-                                <p className="text-center text-small">
+                                <p className="text-center text-small ">
                                     Need a connection example?{" "}
-                                    <Link size="sm" onPress={() => setSelected("sign-up")}>
+                                    <Link size="sm" onPress={() => setSelected("Url")}>
                                         Learn More
                                     </Link>
                                 </p>
 
                             </form>
                         </Tab>
-                        <Tab key="sign-up" title="Sign up">
+                        <Tab key="url" title="Url">
                             <form className="flex flex-col gap-4 h-[300px]">
                                 <Input isRequired label="Name" placeholder="Enter your names" type="password"/>
                                 <Input isRequired label="Email" placeholder="Enter your email" type="email"/>
@@ -137,19 +135,48 @@ export default function Dashboards({}) {
                                 />
                                 <p className="text-center text-small">
                                     Already have an account?{" "}
-                                    <Link size="sm" onPress={() => setSelected("login")}>
-                                        Login
+                                    <Link size="sm" onPress={() => setSelected("host")}>
+                                        Host
                                     </Link>
                                 </p>
-                                <div className="flex gap-2 justify-end">
-                                    <Button fullWidth color="primary">
-                                        Sign up
-                                    </Button>
-                                </div>
+
                             </form>
                         </Tab>
                     </Tabs>
                 </CardBody>
+                <CardFooter className={"m-0"}>
+                    <div className="w-full inline-flex flex-col gap-1">
+                        {/*<Button*/}
+                        {/*    className={"flex-1"}*/}
+                        {/*    size={"md"}*/}
+                        {/*    variant="ghost"*/}
+
+                        {/*>*/}
+                        {/*    Cancel*/}
+                        {/*</Button>*/}
+                    {/*<div className={"flex flex-row gap-5"}>*/}
+                        <Button
+                            isDisabled={false}
+                            className={"flex-2"}
+                            size={"md"}
+                            variant="shadow"
+                            color="default" isLoading={false}
+                        >
+                            Test Connection
+                        </Button>
+                        <Button
+                            isDisabled={false}
+                            className={"flex-2"}
+                            size={"md"}
+                            variant="shadow"
+                            color="primary" isLoading={false}
+                        >
+                            Save Connection
+                        </Button>
+                    {/*</div>*/}
+                    </div>
+
+                </CardFooter>
             </Card>
         </div>
     )
