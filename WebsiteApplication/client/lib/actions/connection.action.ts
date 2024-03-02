5cbb8cc91aa7f5d7e049f6c01d9a84b377e8fede
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-02 15:40:31 +0300

    gave toaster a polish:WebsiteApplication/client/lib/actions/connection.action.ts

--- WebsiteApplication/client/lib/actions/connection.action.ts
+++ WebsiteApplication/client/lib/actions/connection.action.ts
@@ -1,5 +1,5 @@
 import { toast } from "sonner";
-import axios from "axios";
+import axios, { AxiosError, isAxiosError } from "axios";
 
 interface testHostConnectionParams {
 		HostDatabaseName: string;
@@ -19,14 +19,17 @@ export async function testHostConnectionRequest(params: testHostConnectionParams
 				const response = await axios.post(url, params);
 
 				if (response.status === 200) {
-						// toast.success(response.statusText, {
-						// 		id: toastId,
-						// 		position: "top-center"
-						// });
-						toast.success(`Connection Successful ${JSON.stringify(response.data, null, 2)}`, {
+
+						let content = JSON.stringify(response.data, null, 2);
+						if (content.length > 200) {
+								content = content.substring(0, 197) + "...";
+						}
+
+						toast.success("Connection Successful", {
 								id: toastId,
-								duration: 1000,
-								position: "bottom-right"
+								duration: 2000,
+								position: "bottom-right",
+								description: content
 						});
 						return response.data;
 				} else {
@@ -37,43 +40,26 @@ export async function testHostConnectionRequest(params: testHostConnectionParams
 						});
 
 				}
-		} catch (error) {
-				console.error("Axios error:", error);
-				toast.error(`Axios error! status: ${error}`, {
+		} catch (error: AxiosError | any) {
+				let content: string = "Unknown error occurred";
+				let statusCode: number = 500;
+				if (isAxiosError(error)) {
+						statusCode = error.response?.status || 500;
+						let jsonContent = error.response?.data || error.message;
+						// if content length is greater than 100, truncate it
+						content = JSON.stringify(jsonContent, null, 2);
+						if (content.length > 200) {
+								content = content.substring(0, 197) + "...";
+						}
+				} else {
+						content = String(error); // Convert unknown to string
+				}
+
+				toast.error(`Error ${statusCode}`, {
 						id: toastId,
 						duration: 3000,
-						position: "bottom-right"
+						position: "bottom-right",
+						description: content
 				});
 		}
 }
-
-// export async function testHostConnectionRequest(params: testHostConnectionParams) {
-// 		const url = "http://localhost:5000/api/testHostConnection";
-//
-// 		const options = {
-// 				method: "POST",
-// 				body: JSON.stringify({ ...params })
-// 		};
-//
-// 		try {
-// 				const response = await fetch(url, options);
-//
-// 				if (!response.ok) {
-// 						return new Error(`HTTP error! status: ${response.status}`);
-// 				}
-//
-// 				const contentType = response.headers.get("Content-Type");
-// 				if (contentType && contentType.includes("application/json")) {
-// 						return await response.json(); // Parse JSON response
-// 						// console.log(data);
-// 				} else {
-// 						const text = await response.text(); // Read response as text
-// 						console.log("Response is not JSON:", text);
-// 						return new Error("Unexpected response format");
-// 				}
-// 		} catch (error) {
-// 				console.error("Fetch error:", error);
-// 				throw new Error(`HTTP error! status: ${error}`);
-//
-// 		}
-// }
