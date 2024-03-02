9a69d4fb5beea84604e0e793636e0a41b2a58b04
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-02 11:52:04 +0300

    loading icon sonner

--- WebsiteApplication/client/app/layout.tsx
+++ WebsiteApplication/client/app/layout.tsx
@@ -1,38 +1,48 @@
 import React from "react";
-import {Ubuntu} from "next/font/google";
-import type {Metadata} from "next";
+import { Ubuntu } from "next/font/google";
+import type { Metadata } from "next";
 import "./globals.css";
-import {Providers} from "./providers";
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Providers } from "./providers";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Toaster } from "sonner";
+import Loader from "@/components/icons/LoadingIcon";
 
 const ubuntu = Ubuntu({
-    subsets: ["latin"],
-    weight: ["300", "400", "500", "700"], // Include all the weights you defined
-    style: ['normal', 'italic'], // Include both normal and italic styles
-    variable: "--font-ubuntu",
+		subsets: ["latin"],
+		weight: ["300", "400", "500", "700"], // Include all the weights you defined
+		style: ["normal", "italic"], // Include both normal and italic styles
+		variable: "--font-ubuntu"
 });
 
 export const metadata: Metadata = {
-    title: "JettyDash",
-    description:
-        "JettyDash is a modern, open-source dashboard creator for your business.",
-    icons: {
-        icon: "/assets/images/site-logo.svg",
-    },
+		title: "JettyDash",
+		description:
+				"JettyDash is a modern, open-source dashboard creator for your business.",
+		icons: {
+				icon: "/assets/images/site-logo.svg"
+		}
 };
 
-export default function RootLayout({children,}: {
-    children: React.ReactNode;
+export default function RootLayout({ children }: {
+		children: React.ReactNode;
 }) {
-    return (
-        <html lang="en" suppressHydrationWarning>
+		return (
+				<html lang="en" suppressHydrationWarning>
 
-        <body className={`${ubuntu.variable}`}>
+				<body className={`${ubuntu.variable}`}>
 
-        <Providers>{children}<SpeedInsights /></Providers>
+				<Providers>
+						{children}
+						<Toaster position="bottom-right"
+						         icons={{
+								         loading: <Loader visible={true} />
+						         }}
+						/>
+						<SpeedInsights />
+				</Providers>
 
 
-        </body>
-        </html>
-    );
+				</body>
+				</html>
+		);
 }
