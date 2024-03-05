0b8f94674662a2375a39f0489b90da528b3498a7
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-05 03:22:04 +0300

    dockerized and mysql and vault:BackendApplication/Api/Health/HashiCorpVaultHealthCheck.cs

--- BackendApplication/Api/Health/HashiCorpVaultHealthCheck.cs
+++ BackendApplication/Api/Health/HashiCorpVaultHealthCheck.cs
@@ -1,30 +1,15 @@
-
-
-
 using Microsoft.Extensions.Diagnostics.HealthChecks;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using VaultSharp;
-using VaultSharp.V1.AuthMethods.Token;
 namespace Api.Health;
 
-public class HashiCorpVaultHealthCheck : IHealthCheck
+public class HashiCorpVaultHealthCheck(IVaultClient vaultClient) : IHealthCheck
 {
-    private readonly IVaultClient _vaultClient;
-
-    public HashiCorpVaultHealthCheck(string vaultAddress, string token)
-    {
-        var vaultClientSettings = new VaultClientSettings(vaultAddress, new TokenAuthMethodInfo(token));
-        _vaultClient = new VaultClient(vaultClientSettings);
-    }
-
     public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
     {
         try
         {
             // You can customize this based on the health endpoint of your Vault instance.
-            var healthStatus = await _vaultClient.V1.System.GetHealthStatusAsync();
+            var healthStatus = await vaultClient.V1.System.GetHealthStatusAsync();
 
             // Check the health status here and return HealthCheckResult accordingly.
             if (healthStatus?.Initialized ?? false)
