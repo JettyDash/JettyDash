6ebd4962e803bee0593b43d52f597dd9ce6dd49e
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-06 02:35:20 +0300

    mysql migration:BackendApplication/Infrastructure/Seeding/UserSeedData.cs

--- BackendApplication/Infrastructure/Seeding/UserSeedData.cs
+++ BackendApplication/Infrastructure/Seeding/UserSeedData.cs
@@ -1,6 +1,28 @@
+
+using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using Schemes.Enums;
+
 namespace Infrastructure.Seeding;
 
-public class UserSeedData
-{
-    
-}
+
+
+public static class UserSeedData
+    {
+        public static void SeedUsers(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    UserId = 1,
+                    Username = "guest",
+                    Password = "null",
+                    Email = "null",
+                    Role = Role.Guest,
+                    PasswordRetryCount = 0,
+                    IsActive = true,
+                    LastActivityDateTime = DateTime.UtcNow,
+                }
+            );
+        }
+    }
