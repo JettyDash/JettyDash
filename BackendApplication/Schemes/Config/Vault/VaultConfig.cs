718f96b5045341bed3430496093904a503127da1
Riza Semih Koca <rsemihkoca@outlook.com>
2024-03-05 03:22:04 +0300

    dockerized and mysql and vault:BackendApplication/Schemes/Config/Vault/VaultConfig.cs

--- BackendApplication/Schemes/Config/Vault/VaultConfig.cs
+++ BackendApplication/Schemes/Config/Vault/VaultConfig.cs
@@ -3,7 +3,8 @@ namespace Schemes.Config.Vault;
 public class VaultConfig
 {
     public string Address { get; set;}
-    public string Token { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
     public int EngineVersion { get; set; }
     public string Mount { get; set; }
 }
