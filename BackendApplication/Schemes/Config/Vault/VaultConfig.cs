7609af0cbccbf9841732efae6d33748ec58ae383
Riza Semih Koca <rsemihkoca@outlook.com>
2024-04-22 15:27:34 +0300

    commented vault it will be reduced:BackendApplication/Schemes/Config/Vault/VaultConfig.cs

--- BackendApplication/Schemes/Config/Vault/VaultConfig.cs
+++ BackendApplication/Schemes/Config/Vault/VaultConfig.cs
@@ -1,10 +1,10 @@
-namespace Schemes.Config.Vault;
-
-public class VaultConfig
-{
-    public string Address { get; set;}
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public int EngineVersion { get; set; }
-    public string Mount { get; set; }
-}
+// namespace Schemes.Config.Vault;
+//
+// public class VaultConfig
+// {
+//     public string Address { get; set;}
+//     public string Username { get; set; }
+//     public string Password { get; set; }
+//     public int EngineVersion { get; set; }
+//     public string Mount { get; set; }
+// }
